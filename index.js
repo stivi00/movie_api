0318de969e174cbe04a146cb33526091ee707ee3
stivi00 <stivisth@gmail.com>
2023-09-01 11:16:50 +0200

    delete user endpoint

--- index.js
+++ index.js
@@ -150,17 +150,19 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
 });
 
 //DELETE
-app.delete('/users/:id/', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find((user) => user.id == id);
-
-    if (user) {
-        users = users.filter((user) => user.id != id);
-        res.status(200).send(`user ${id} has been removed`);
-    } else {
-        res.status(400).send('User not found');
-    }
+app.delete('/users/:Username/', async (req, res) => {
+    await Users.findOneAndDelete({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
 });
 
 //READ
