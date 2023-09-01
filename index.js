616ff720ac6aacde37c01a6fb04cd119c646378a
stivi00 <stivisth@gmail.com>
2023-09-01 10:03:56 +0200

    update user endpoint implemented

--- index.js
+++ index.js
@@ -176,18 +176,26 @@ app.get('/users/:Username', async (req, res) => {
 });
 
 //UPDATE
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find((user) => user.id == id);
-
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('User not found');
-    }
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        {
+            $set: {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday,
+            },
+        },
+        { new: true }
+    )
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //POST
