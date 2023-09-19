505b8e3e95e380035785495710074d09ed21dddc
stivi00 <stivisth@gmail.com>
2023-09-19 13:17:10 +0200

    removed authorization to get movies

--- index.js
+++ index.js
@@ -220,18 +220,14 @@ app.delete(
 );
 
 //READ
-app.get(
-    '/movies',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-        await Movies.find()
-            .then((movies) => res.status(200).send(movies))
-            .catch((err) => {
-                console.error(err);
-                res.status(500).send('Error: ' + err);
-            });
-    }
-);
+app.get('/movies', async (req, res) => {
+    await Movies.find()
+        .then((movies) => res.status(200).send(movies))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 //READ
 app.get(
